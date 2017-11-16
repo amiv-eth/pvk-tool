c77c2277bd2bc809fbc308d5e4a4d89f9c317f98
Alexander Dietmüller <alexander@dietmueller.de>
2017-11-16 02:33:16 +0100

    Add a demo dev server for easier frontend development.

---
+++ Backend/demo_server.py
@@ -0,0 +1,164 @@
+"""Demo Server.
+
+Add some demo data to the database and run the app.
+"""
+
+from os import getenv
+from random import randint
+from datetime import datetime as dt, timedelta
+import json
+
+from flask import g
+
+from app import create_app
+
+
+HOST = getenv('PVK_HOST', '0.0.0.0')
+PORT = getenv('PVK_PORT', '80')
+
+APP = create_app()
+CLIENT = APP.test_client()
+DATE_FORMAT = APP.config['DATE_FORMAT']
+ASSISTANTS = ['pablo', 'assi', 'anon', 'mongo']
+MIN_SPOTS = 20
+MAX_SPOTS = 40
+
+"""
+# Clear everything
+from pymongo import MongoClient
+
+connection = MongoClient(APP.config['MONGO_HOST'],
+                         APP.config['MONGO_PORT'])
+connection.drop_database(APP.config['MONGO_DBNAME'])
+connection.close()
+"""
+
+
+def post(resource, data):
+    """Create something, ignoring auth."""
+    with APP.test_request_context():
+        g.user = 'Not None :)'
+        g.nethz = 'Something'
+        g.admin = True
+
+        response = CLIENT.post(resource,
+                               data=json.dumps(data),
+                               content_type="application/json",
+                               headers={'Authorization': 'Token Lala'})
+
+        if response.status_code != 201:
+            """
+            error = json.loads(response.get_data(as_text=True))
+            status = error['_error']['code']
+            issues = str(error['_issues'])
+            print('Warning(%s):' % status, issues)
+            """
+            return {}
+
+        return json.loads(response.get_data(as_text=True))
+
+
+def create_lectures(department):
+    """Create a few lectures, return names."""
+    lectures = []
+    for lecture in ['Some %s Lecture', 'Cool %s Lecture', 'Boring %s Lecture']:
+        for year in range(1, 3):
+            special_name = '%s Year %s' % (department.upper(), year)
+
+            data = {
+                'title': lecture % special_name,
+                'year': year,
+                'department': department,
+                'assistants': ASSISTANTS,
+            }
+            response = post('lectures', data)
+            if response:
+                lectures.append(response['_id'])
+
+    return lectures
+
+
+def room_gen():
+    """Produce rooms."""
+    i = 0
+    while True:
+        yield "ETZ E %d" % i
+        i += 1
+
+
+def time_gen(starting_day):
+    """Produce a stream of non-overlapping time ranges."""
+    while True:
+        year = starting_day.year
+        month = starting_day.month
+        day = starting_day.day
+
+        # Random start and end time
+        start = dt(year, month, day, randint(7, 10)).strftime(DATE_FORMAT)
+        end = dt(year, month, day, randint(11, 14)).strftime(DATE_FORMAT)
+        yield {'start': start, 'end': end}
+
+        # Only one slot per day to avoid overlap
+        starting_day += timedelta(days=1)
+
+
+ROOM = room_gen()
+TIME = time_gen(dt.now() + timedelta(days=90))  # Sometime in the future
+
+
+def create_course(lecture, assistant, open_signup=True):
+    """Create several course for each lecture."""
+    signup_diff = timedelta(days=-5) if open_signup else timedelta(15)
+    start = dt.utcnow() + signup_diff
+    end = start + timedelta(days=15)
+
+    data = {
+        'lecture': lecture,
+        'assistant': assistant,
+
+        'signup': {
+            'start': start.strftime(DATE_FORMAT),
+            'end': end.strftime(DATE_FORMAT),
+        },
+
+        'datetimes': [next(TIME) for _ in range(randint(1, 3))],
+        'room': next(ROOM),
+        'spots': randint(MIN_SPOTS, MAX_SPOTS),
+    }
+    return post('courses', data)['_id']
+
+
+def create_signups(course):
+    """Create random number of signups to a course."""
+    for n in range(randint(0, 2*MAX_SPOTS)):
+        data = {
+            'nethz': 'student%d' % n,
+            'course': course
+        }
+        post('signups', data=data)
+
+
+if __name__ == '__main__':
+    # Create courses
+    # (Repeat for itet and mavt)
+    for department in ['itet', 'mavt']:
+        # Create a few lectures
+        lectures = create_lectures(department)
+
+        # Create a few courses with closed signup
+        for lecture in lectures:
+            for n in range(randint(0, 2)):
+                create_course(lecture, ASSISTANTS[n], open_signup=False)
+
+        # Create a few courses with open signup
+        courses = []
+        for lecture in lectures:
+            for n in range(randint(2, len(ASSISTANTS))):
+                courses.append(create_course(lecture, ASSISTANTS[n]))
+
+        # Create signups for courses
+        for course in courses:
+            create_signups(course)
+
+    # Start the app
+    APP.run(host=HOST, port=int(PORT))
--- README.md
+++ README.md
@@ -6,6 +6,48 @@ A new AMIV PVK tool using Eve and authenticating users with AMIVAPI.
 
 WIP.
 
+### Starting A Local Backend for Development
+
+Using Docker, you can quickly set up a backend with demo data to focus on
+frontend development. Here's how:
+
+1. [Install Docker](https://www.docker.com/community-edition#/download)
+
+2. You need MongoDB. Luckily, you can just use Docker for that, too.
+   (It's one long command, make sure to copy everything to your command line)
+  
+   ```bash
+   docker run --name mongodb -d -p 27017 \
+     -e MONGODB_DATABASE="pvk" \
+     -e MONGODB_USERNAME="pvkuser" \
+     -e MONGODB_PASSWORD="pvkpass" \
+     bitnami/mongodb:latest
+   ```
+  
+   The environment variables specified with `-e` make sure that the db and user
+   are created
+
+3. Build and start the dev-container. (Two commands this time)
+   Adjust the path `./Backend` at the end as needed so it points to the
+   `backend` directory.
+
+   ```bash
+   docker build -t pvk ./Backend/
+   ```
+
+   ```bash
+   docker run --name pvk -d -p 80:80 \
+     --link mongodb \
+     -e MONGO_HOST="mongodb" \
+     pvk
+   ```
+   The `--link` and `-e` make sure the backend can access the db container.
+
+And now the backend is available at port 80, ready to use!
+
+Use the commands `docker start pvk` / `docker stop pvk` and
+`docker start mongodb` / `docker stop mongodb` to control your containers.
+
 
 ## Backend
 
@@ -35,23 +77,23 @@ But don't worry, it's very easy to set them up -- take a look at the
   and `readWrite` permission to the `pvk_test` database.
 
   On Linux or similar, you can use this one-liner:
-  ```
+  
+  ```bash
   mongo pvk_test --eval 'db.createUser({user:"pvk_user",pwd:"pvk_pass",roles:["readWrite"]});'
   ```
 
 - Secondly, set up your virtual environment, install requirements as well as
   [py.test](https://docs.pytest.org/en/latest/).
 
-  ```
+  ```bash
   python -m venv env
   source env/bin/activate
-
   pip install -r requirements.txt
   pip install pytest
   ```
 
 - Finally, you can run the tests from your virtual environment with:
 
-  ```
+  ```bash
   py.test
   ```
