446452004931cd77232c608b822aa53d30d550f2
Alexander Dietmüller <alexander@dietmueller.de>
2017-11-28 00:18:23 +0100

    Frontend: Remove unused css classes

--- Frontend/src/PvkList.js
+++ Frontend/src/PvkList.js
@@ -2,46 +2,33 @@ import m from 'mithril';
 
 import Pvk from './Pvk';
 
-class PvkList {
+module.exports = {
   oninit() {
-    this.m = m;
     Pvk.loadlist();
     m.redraw();
-  }
+  },
   view() {
-    return this.m(
-      '.pvk-list',
-      this.m(
-        'table',
-        m(
-          'thead',
-          m(
-            'tr',
-            m('th', 'Course'),
-            m('th', 'Department'),
-            m('th', 'Name'),
-            m('th', 'Starting time'),
-            m('th', 'Ending time'),
-          ),
-        ),
-        m(
-          'tbody',
-          Pvk.list.map(pvk => m(
-            'tr.pvk-list-item',
-            m('td.pvk-list-item-title', pvk.lecture.title),
-            m('td.pvk-list-item-department', pvk.lecture.department),
-            m('td.pvk-list-item-assistant-name', pvk.assistant.name),
-            pvk.datetimes.map(timeslot => [m('td.pvk-list-item-timeslot', timeslot.start),
-              m('td.pvk-list-item-timeslot', timeslot.end)]),
-            m(
-              'td.pvk-list-btn ',
-              m('button', 'add course'),
-            ),
-          )),
-        ),
-      ),
-    );
-  }
-}
-
-export default new PvkList();
+    return m('table', [
+      m('thead', [
+        m('tr', [
+          m('th', 'Course'),
+          m('th', 'Department'),
+          m('th', 'Name'),
+          m('th', 'Starting time'),
+          m('th', 'Ending time'),
+        ]),
+      ]),
+      m('tbody', Pvk.list.map(pvk =>
+        m('tr', [
+          m('td', pvk.lecture.title),
+          m('td', pvk.lecture.department),
+          m('td', pvk.assistant.name),
+          pvk.datetimes.map(timeslot => [
+            m('td', timeslot.start),
+            m('td', timeslot.end),
+          ]),
+          m('td', m('button', 'add course')),
+        ]))),
+    ]);
+  },
+};
