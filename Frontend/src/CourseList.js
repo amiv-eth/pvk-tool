d0108f0a07c50a8c5a739ab8184a461e6dfc0157
Alexander Dietmüller <alexander@dietmueller.de>
2017-12-02 00:03:13 +0100

    Frontend: Integrate actual API requests into CourseView

--- Frontend/src/CourseList.js
+++ Frontend/src/CourseList.js
@@ -1,28 +1,31 @@
-import PvkList from './PvkList';
-
 const m = require('mithril');
-const { Courses, UserCourses } = require('./api.js');
+const { Courses } = require('./api.js');
 
 module.exports = {
   oninit() { Courses.load(); },
 
   view() {
-<<<<<<< HEAD
-    return m('div', [m(PvkList)]);
-=======
-    // return m('div', 'Hellooo!');
-    return m('ul', [
-      Courses.list.map(course =>
-        m('li', [
-          m('p', `${course.lecture.title}, ${course.assistant}`),
-          m(
-            'button',
-            { onclick() { UserCourses.select(course); } },
-            'Add Course',
-          ),
-        ])),
+    return m('table', [
+      m('thead', [
+        m('tr', [
+          m('th', 'Course'),
+          m('th', 'Department'),
+          m('th', 'Name'),
+          m('th', 'Starting time'),
+          m('th', 'Ending time'),
+        ]),
+      ]),
+      m('tbody', Courses.list.map(course =>
+        m('tr', [
+          m('td', course.lecture.title),
+          m('td', course.lecture.department),
+          m('td', course.assistant.name),
+          course.datetimes.map(timeslot => [
+            m('td', timeslot.start),
+            m('td', timeslot.end),
+          ]),
+          m('td', m('button', 'add course')),
+        ]))),
     ]);
-    // m('button', { onclick() { Courses.select(); } }, 'Add Course');
->>>>>>> Frontend: Talk to backend and display courses in sidebar
   },
 };
